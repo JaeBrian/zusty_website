08869ed1d794364c9aabdb3dc8256f22a8f3fb54
Dana Kaplan <danakaplan96@gmail.com>
2023-12-04 18:38:37 -0500

    team section

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,67 +1,70 @@
-// const people = [
-//   {
-//     name: 'Dana Kaplan',
-//     role: (
-//       <>
-//         <a href="https://github.com/DanaKaplan944">GitHub</a>,{' '}
-//         <a href="https://www.linkedin.com/in/danakaplan2/">LinkedIn</a>
-//       </>
-//     ),
-//     imageUrl:
-//       'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-//   },
-//   {
-//     name: 'Nancy Huang',
-//     role: (
-//       <>
-//         <a href="https://github.com/nancy-huang">GitHub</a>,{' '}
-//         <a href="https://www.linkedin.com/in/nancy-huang/">LinkedIn</a>
-//       </>
-//     ),
-//     imageUrl:
-//       'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-//   },
-//   {
-//     name: 'Brian Lee',
-//     role: (
-//       <>
-//         <a href="https://github.com/brian-lee">GitHub</a>,{' '}
-//         <a href="https://www.linkedin.com/in/brian-lee/">LinkedIn</a>
-//       </>
-//     ),
-//     imageUrl:
-//       'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-//   },
-//   {
-//     name: 'Adrian Insingo',
-//     role: (
-//       <>
-//         <a href="https://github.com/adrian-insingo">GitHub</a>,{' '}
-//         <a href="https://www.linkedin.com/in/adrian-insingo/">LinkedIn</a>
-//       </>
-//     ),
-//     imageUrl:
-//       'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-//   },
-//   {
-//     name: 'William Kill',
-//     role: (
-//       <>
-//         <a href="https://github.com/william-kill">GitHub</a>,{' '}
-//         <a href="https://www.linkedin.com/in/william-kill/">LinkedIn</a>
-//       </>
-//     ),
-//     imageUrl:
-//       'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-//   },
-// ];
+const people = [
+  {
+    name: 'Adrian Insingo',
+    role: (
+      <>
+        <a href='https://github.com/adrian-insingo'>GitHub</a> {' | '}
+        <a href='https://www.linkedin.com/in/adrian-insingo/'>LinkedIn</a>
+      </>
+    ),
+    imageUrl:
+      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+  {
+    name: 'Brian Lee',
+    role: (
+      <>
+        <a href='https://github.com/brian-lee'>GitHub</a> {' | '}
+        <a href='https://www.linkedin.com/in/brian-lee/'>LinkedIn</a>
+      </>
+    ),
+    imageUrl:
+      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+  {
+    name: 'Dana Kaplan',
+    role: (
+      <>
+        <a href='https://github.com/DanaKaplan944'>GitHub</a> {' | '}
+        <a href='https://www.linkedin.com/in/danakaplan2/'>LinkedIn</a>
+      </>
+    ),
+    imageUrl:
+      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+  {
+    name: 'Nancy Huang',
+    role: (
+      <>
+        <a href='https://github.com/nancy-huang'>GitHub</a> {' | '}
+        <a href='https://www.linkedin.com/in/nancy-huang/'>LinkedIn</a>
+      </>
+    ),
+    imageUrl: '/src/assets/nancy.jpg',
+  },
+  {
+    name: 'William Kill',
+    role: (
+      <>
+        <a href='https://github.com/william-kill'>GitHub</a> {' | '}
+        <a href='https://www.linkedin.com/in/william-kill/'>LinkedIn</a>
+      </>
+    ),
+    imageUrl:
+      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+];
 
-const Team = () => {
+export default function Team() {
   return (
-    <div className="page">
-      <div>TEAM PAGE, CIRCLE IMAGE OF PROFILES, GITHUB LINK CONTACT</div>
+    <div className='teamContainer'>
+      {people.map((person) => (
+        <div key={person.name}>
+          <img className='personImg' src={person.imageUrl} />
+          <p>{person.name}</p>
+          <p>{person.role}</p>
+        </div>
+      ))}
     </div>
   );
-};
-
-export default Team;
+}
