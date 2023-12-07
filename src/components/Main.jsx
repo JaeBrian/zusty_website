3e5c41b23671b69d59726da7686b97756cd1e8c4
Brian Lee <bnlee419@gmail.com>
2023-12-07 11:16:57 -0800

    zustywebsite mainpage

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,19 @@
+import './MainNav.css';
+
 export default function Main() {
-  //chrome store, github files npm
   return (
-    <div className="page">
-      <div>GITHUB, NPM, CHROME STORE, NAV BAR</div>
+    <div className="page-x">
+      <div className="logo-container">
+        <div className="logo"></div>
+        <div className="intro">
+          <p className="paragraph">
+            <p>WHAT IS ZUSTY</p>
+            Zusty is a Zustand developer tool that allows you to time travel,
+            look at state snapshots for when developing a Zustand web
+            application
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
